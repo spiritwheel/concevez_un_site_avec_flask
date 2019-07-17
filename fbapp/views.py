970fa6ae9ade0935977816deafd516ffb8650a14
spiritwheel <fernandez.logan@yahoo.fr>
2019-07-18 00:01:17 +0200

    modif view.py pour passer des paramétre dans le render_template de la fonction result

--- fbapp/views.py
+++ fbapp/views.py
@@ -11,15 +11,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html')
+    description = """
+        Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)
+    """
+    return render_template('index.html',
+                            user_name='Julio',
+                            user_image=url_for('static', filename='img/profile.png'),
+                            description=description,
+                            blur=True)
 
 @app.route('/result/')
 def result():
     description = """
             Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée ! Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !
         """
-    return render_template('result.html',user_name="Tom",user_image=url_for('static', filename='tmp/cover_111823112767411.jpg'),description=description,
-                            blur=True)
+    return render_template('result.html',user_name="Tom",user_image=url_for('static', filename='tmp/cover_111823112767411.jpg'),description=description)
 
 # if __name__ == "__main__":
 #     app.run()
